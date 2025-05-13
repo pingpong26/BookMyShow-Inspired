3aa63b5149b0160f8e4d2de19271fd395e62ecd1
moin-ullah <moinullah02@gmail.com>
2025-05-13 23:26:24 +0530

    Add bcrypt password hashing middleware to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,5 +27,20 @@ const userSchema = new mongoose.Schema(
     timestamps: true, // Automatically adds createdAt and updatedAt
   }
 );
+
+// 🔐 Password Hashing Middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next(); // Skip if password wasn't changed
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
